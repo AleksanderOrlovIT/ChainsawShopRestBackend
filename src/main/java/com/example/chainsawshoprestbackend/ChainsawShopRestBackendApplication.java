e86a59cb5628881b5f68faed66e9342c9b997bd4
AleksanderOrlovIT <alexanderorlovkh@gmail.com>
2023-06-12 12:00:22 +0300

    Initial commit

---
+++ src/main/java/com/example/chainsawshoprestbackend/ChainsawShopRestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.chainsawshoprestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainsawShopRestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChainsawShopRestBackendApplication.class, args);
+    }
+
+}
