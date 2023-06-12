6083c41c46e18a657cb9a2a0cba7487c0b524c34
AleksanderOrlovIT <alexanderorlovkh@gmail.com>
2023-06-12 12:48:41 +0300

    Created Brand Entity. Made relations between Brand and Chainsaw. Closes #1.

---
+++ src/main/java/com/example/chainsawshoprestbackend/model/Brand.java
@@ -0,0 +1,41 @@
+package com.example.chainsawshoprestbackend.model;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Brand extends BaseEntity{
+
+    @Builder
+    public Brand(Long id, String name, String url, String email, Set<Chainsaw> chainsaws, Byte[] image) {
+        super(id);
+        this.name = name;
+        this.url = url;
+        this.email = email;
+        this.image = image;
+        if(chainsaws != null)
+            this.chainsaws = chainsaws;
+    }
+
+    private String name;
+
+    private String url;
+
+    private String email;
+
+    private Byte[] image;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "brand")
+    private Set<Chainsaw> chainsaws;
+}
--- src/main/java/com/example/chainsawshoprestbackend/model/Chainsaw.java
+++ src/main/java/com/example/chainsawshoprestbackend/model/Chainsaw.java
@@ -1,6 +1,9 @@
 package com.example.chainsawshoprestbackend.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,11 +14,23 @@ import lombok.Setter;
 @Entity
 public class Chainsaw extends BaseEntity{
 
-    private String Model;
+    @Builder
+    public Chainsaw(Long id, String modelName, Integer price, Byte[] image, Brand brand) {
+        super(id);
+        this.ModelName = modelName;
+        this.price = price;
+        this.image = image;
+        this.brand = brand;
+    }
+
+    private String ModelName;
 
     private Integer price;
 
     private Byte[] image;
+
+    @ManyToOne
+    private Brand brand;
 }
 
 
